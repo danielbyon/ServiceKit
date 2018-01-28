9b5f03b9a26d9775820da56c4a011a639b2307ce
Daniel Byon <danielbyon@gmail.com>
2018-01-27 23:20:22 -0800

    Fix completion handler stacking

--- ServiceKit/Utilities/RequestQueue.swift
+++ ServiceKit/Utilities/RequestQueue.swift
@@ -65,50 +65,70 @@ public class RequestQueue: RequestQueueProtocol {
 
     @discardableResult
     public func performRequest<T: NetworkRequest>(_ request: T, completion: ((Result<T.ResultType>) -> Void)?) -> Operation where T.ResultType == Void {
-        if request.shouldCoalesceMultipleCompletions,
-            let existingOperation = checkForExistingOperation(request, completion: completion) {
-            return existingOperation
-        }
+        var returnOperation: RequestQueueOperation<T>?
+        callbackManipulationQueue.sync { [unowned self] in
+            if request.shouldCoalesceMultipleCompletions,
+                let existingOperation = self.checkForExistingOperation(request, completion: completion) {
+                returnOperation = existingOperation
+            }
 
-        let operation = NoContentOperation(request: request, session: session)
-        addOperation(operation, completion: completion)
-        return operation
+            let operation = NoContentOperation(request: request, session: session)
+            self.addOperation(operation, completion: completion)
+            returnOperation = operation
+        }
+        assert(returnOperation != nil)
+        return returnOperation!
     }
 
     @discardableResult
     public func performRequest<T: DataRequest>(_ request: T, completion: ((Result<T.ResultType>) -> Void)?) -> Operation {
-        if request.shouldCoalesceMultipleCompletions,
-            let existingOperation = checkForExistingOperation(request, completion: completion) {
-            return existingOperation
-        }
+        var returnOperation: RequestQueueOperation<T>?
+        callbackManipulationQueue.sync { [unowned self] in
+            if request.shouldCoalesceMultipleCompletions,
+                let existingOperation = self.checkForExistingOperation(request, completion: completion) {
+                returnOperation = existingOperation
+            }
 
-        let operation = DataOperation(request: request, session: session)
-        addOperation(operation, completion: completion)
-        return operation
+            let operation = DataOperation(request: request, session: session)
+            self.addOperation(operation, completion: completion)
+            returnOperation = operation
+        }
+        assert(returnOperation != nil)
+        return returnOperation!
     }
 
     @discardableResult
     public func performRequest<T: ParsableRequest>(_ request: T, completion: ((Result<T.ResultType>) -> Void)?) -> Operation {
-        if request.shouldCoalesceMultipleCompletions,
-            let existingOperation = checkForExistingOperation(request, completion: completion) {
-            return existingOperation
-        }
+        var returnOperation: RequestQueueOperation<T>?
+        callbackManipulationQueue.sync { [unowned self] in
+            if request.shouldCoalesceMultipleCompletions,
+                let existingOperation = self.checkForExistingOperation(request, completion: completion) {
+                returnOperation = existingOperation
+            }
 
-        let operation = JSONOperation(request: request, session: session)
-        addOperation(operation, completion: completion)
-        return operation
+            let operation = JSONOperation(request: request, session: session)
+            self.addOperation(operation, completion: completion)
+            returnOperation = operation
+        }
+        assert(returnOperation != nil)
+        return returnOperation!
     }
 
     @discardableResult
     public func performRequest<T: ParsableArrayRequest>(_ request: T, completion: ((Result<T.ResultType>) -> Void)?) -> Operation {
-        if request.shouldCoalesceMultipleCompletions,
-            let existingOperation = checkForExistingOperation(request, completion: completion) {
-            return existingOperation
-        }
+        var returnOperation: RequestQueueOperation<T>?
+        callbackManipulationQueue.sync { [unowned self] in
+            if request.shouldCoalesceMultipleCompletions,
+                let existingOperation = self.checkForExistingOperation(request, completion: completion) {
+                returnOperation = existingOperation
+            }
 
-        let operation = JSONArrayOperation(request: request, session: session)
-        addOperation(operation, completion: completion)
-        return operation
+            let operation = JSONArrayOperation(request: request, session: session)
+            self.addOperation(operation, completion: completion)
+            returnOperation = operation
+        }
+        assert(returnOperation != nil)
+        return returnOperation!
     }
 
     public func requestCurrentlyExecuting<T: Request>(_ request: T) -> Operation? {
@@ -118,77 +138,71 @@ public class RequestQueue: RequestQueueProtocol {
 
     // MARK: Private Methods
 
-    private func checkForExistingOperation<T: Request>(_ request: T, completion: ((Result<T.ResultType>) -> Void)?) -> Operation? {
+    private func checkForExistingOperation<T: Request>(_ request: T, completion: ((Result<T.ResultType>) -> Void)?) -> RequestQueueOperation<T>? {
         let callbackIdentifier = request.identifier
-        var existingOperation: Operation?
-        callbackManipulationQueue.sync {
-            guard let currentOperation = self.requestCurrentlyExecuting(request),
-                let wrappedCompletion = self.callbacks[callbackIdentifier] else {
-                return
-            }
-
-            let rewrappedCompletion: CallbackWrapperBlock = { result in
-                // Call the existing completion first
-                wrappedCompletion(result)
-
-                // Then call the new completion
-                switch result {
-                case .success(let value as T.ResultType):
-                    completion?(.success(value))
-                case .failure(let error):
-                    completion?(.failure(error))
-                case .success(let value):
-                    completion?(.failure(RequestQueueError.identifierMismatch(identifier: callbackIdentifier, expectedType: T.ResultType.self, actualType: type(of: value))))
-                }
-            }
-            self.callbacks[callbackIdentifier] = rewrappedCompletion
-            existingOperation = currentOperation
+        guard let existingOperation = self.requestCurrentlyExecuting(request) as? RequestQueueOperation<T>,
+            let wrappedCompletion = self.callbacks[callbackIdentifier] else {
+                return nil
         }
+
+        let rewrappedCompletion: CallbackWrapperBlock = { result in
+            // Call the existing completion first
+            wrappedCompletion(result)
+
+            // Then call the new completion
+            switch result {
+            case .success(let value as T.ResultType):
+                completion?(.success(value))
+            case .failure(let error):
+                completion?(.failure(error))
+            case .success(let value):
+                completion?(.failure(RequestQueueError.identifierMismatch(identifier: callbackIdentifier, expectedType: T.ResultType.self, actualType: type(of: value))))
+            }
+        }
+        self.callbacks[callbackIdentifier] = rewrappedCompletion
         return existingOperation
     }
 
     private func addOperation<T>(_ operation: RequestQueueOperation<T>, completion: ((Result<T.ResultType>) -> Void)?) {
-        callbackManipulationQueue.sync {
-            operation.completionBlock = { [weak self, weak operation] in
-                guard let strongSelf = self,
-                    let operation = operation,
-                    let result = operation.result else {
+        operation.completionBlock = { [weak self, weak operation] in
+            guard let strongSelf = self,
+                let operation = operation,
+                let result = operation.result else {
                     completion?(.failure(RequestQueueError.operationCancelled))
                     return
-                }
-
-                let callbackIdentifier = operation.request.identifier
-
-                var finalCompletion: CallbackWrapperBlock?
-                strongSelf.callbackManipulationQueue.sync {
-                    finalCompletion = strongSelf.callbacks.removeValue(forKey: callbackIdentifier)
-                }
-                guard let completion = finalCompletion else {
-                    assertionFailure("Missing callback for identifier \(callbackIdentifier), this shouldn't happen.")
-                    return
-                }
-                switch result {
-                case .success(let value):
-                    completion(.success(value))
-                case .failure(let error):
-                    completion(.failure(error))
-                }
             }
 
             let callbackIdentifier = operation.request.identifier
-            let wrappedCompletion: CallbackWrapperBlock = { result in
-                switch result {
-                case .success(let value as T.ResultType):
-                    completion?(.success(value))
-                case .failure(let error):
-                    completion?(.failure(error))
-                case .success(let value):
-                    completion?(.failure(RequestQueueError.identifierMismatch(identifier: callbackIdentifier, expectedType: T.ResultType.self, actualType: type(of: value))))
-                }
+
+            var finalCompletion: CallbackWrapperBlock?
+            strongSelf.callbackManipulationQueue.sync {
+                finalCompletion = strongSelf.callbacks.removeValue(forKey: callbackIdentifier)
             }
-            self.callbacks[callbackIdentifier] = wrappedCompletion
-            self.queue.addOperation(operation)
+            guard let completion = finalCompletion else {
+                assertionFailure("Missing callback for identifier \(callbackIdentifier), this shouldn't happen.")
+                return
+            }
+            switch result {
+            case .success(let value):
+                completion(.success(value))
+            case .failure(let error):
+                completion(.failure(error))
+            }
         }
+
+        let callbackIdentifier = operation.request.identifier
+        let wrappedCompletion: CallbackWrapperBlock = { result in
+            switch result {
+            case .success(let value as T.ResultType):
+                completion?(.success(value))
+            case .failure(let error):
+                completion?(.failure(error))
+            case .success(let value):
+                completion?(.failure(RequestQueueError.identifierMismatch(identifier: callbackIdentifier, expectedType: T.ResultType.self, actualType: type(of: value))))
+            }
+        }
+        self.callbacks[callbackIdentifier] = wrappedCompletion
+        self.queue.addOperation(operation)
     }
 
 }
